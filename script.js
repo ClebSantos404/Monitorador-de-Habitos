902529d44f971b8d8f9119e5d6d05ee3f6d8750a
Clebson Santos <clebsondantas404@gmail.com>
2023-01-20 10:52:10 -0300

    adicionando e brincando com JS

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
 const form = document.querySelector("form")
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector("header button")
 
-const data = {
-    basquet: ["01-01", "01-02", "01-06", "01-07"],
-    water: ["01-01", "01-02", "01-06", "01-07"]
+button.addEventListener('click', add)
+
+function add() {
+
+ // "today" recebe uma nova data, a data vai ser pelo seu local indicado
+ // dentro dos paramentros do "toLocaleDateString()".
+ // vai ser transformada em string formato (pt-br) e recortada pelo "slice".
+    const today = new Date().toLocaleDateString('pt-br').slice(0, 5)
+    const dayExists = nlwSetup.dayExists(today)
+
+    if(dayExists) {
+        alert("O Dia, já existe ❌")
+        return
+    }
+
+ //const "dayExists" verifica dentro do (nlwSetup) se o (today) existe.
+ //se for (false) ou seja não existe, ele pula aqui para o "addDay" adicionando o dia.
+ //se for (true) ele cai na condição "if (dayExists)", return finaliza o código. 
+   alert('Dia adicionado com sucesso ✅')
+    nlwSetup.addDay(today)
 }
 
-nlwSetup.setData(data)
-nlwSetup.load()
+// const data = {
+
+//     basquet: ["01-01", "01-02", "01-06", "01-07"],
+//     water: ["01-01", "01-02", "01-06", "01-07"]
+// }
+
+// nlwSetup.setData(data)
+// nlwSetup.load()
